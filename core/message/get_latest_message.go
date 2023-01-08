b09964ce6d10267163db643872d1f791828d20c6
STNeto1 <gfreitasneto18@gmail.com>
2023-01-08 00:23:04 -0300

    get latest message

---
+++ core/message/get_latest_message.go
@@ -0,0 +1,69 @@
+package message
+
+import (
+	cq "_core/queue"
+	lib "_lib"
+	"_models/ent"
+	"_models/ent/message"
+	"_models/ent/queue"
+	"_models/ent/schema"
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+)
+
+type GetLatestMessagePayload struct {
+	QueueID uuid.UUID
+	User    *ent.User
+}
+
+func (m *MessageService) GetLatestMessage(ctx context.Context, payload *GetLatestMessagePayload) (*ent.Message, error) {
+	q, err := m.qs.ShowQueue(ctx, cq.ShowQueuePayload{
+		ID:   payload.QueueID,
+		User: payload.User,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	tx, err := m.client.BeginTx(ctx, &sql.TxOptions{
+		Isolation: sql.LevelSerializable,
+	})
+	if err != nil {
+		m.logger.Error("failed to create transaction", zap.Error(err))
+		return nil, err
+	}
+
+	msg, err := tx.Message.
+		Query().
+		Where(message.HasQueueWith(queue.IDEQ(q.ID))).
+		Where(message.Status(schema.QueueMessageStatusPending)).
+		Where(message.AvailableFromLTE(time.Now())).
+		Order(ent.Desc(message.FieldAvailableFrom)).
+		First(ctx)
+	if err != nil {
+		m.logger.Error("failed to get latest message", zap.Error(err))
+		return nil, lib.Rollback(tx, errors.New("failed to get latest message"))
+	}
+
+	_, err = tx.Message.
+		UpdateOne(msg).
+		SetStatus(schema.QueueMessageStatusProcessing).
+		Save(ctx)
+	if err != nil {
+		m.logger.Error("failed to update message status", zap.Error(err))
+		return nil, lib.Rollback(tx, errors.New("failed to update message status"))
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		m.logger.Error("failed to commit transaction", zap.Error(err))
+		return nil, err
+	}
+
+	return msg, nil
+}
---
+++ core/message/get_latest_messate_test.go
@@ -0,0 +1,71 @@
+package message_test
+
+import (
+	"_core/message"
+	"_models/ent"
+	"_models/ent/schema"
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func createQueueMessage(client *ent.Client, usr *ent.User, q *ent.Queue, body string) (*ent.Message, error) {
+	return client.Message.Create().
+		AddQueue(q).
+		SetBody(body).
+		SetContentType("any content type").
+		SetMaxRetries(5).
+		SetAvailableFrom(time.Now()).
+		Save(context.Background())
+}
+
+func TestErrorWhenReadingQueueWithNoMessages(t *testing.T) {
+	s, client, l := CreateMessageService(t)
+	defer client.Close()
+	defer l.Sync()
+
+	usr, err := createUser(client)
+	assert.NotNil(t, usr)
+	assert.NoError(t, err)
+
+	q, err := createQueue(client, usr, "some-queue")
+	assert.NotNil(t, q)
+	assert.NoError(t, err)
+
+	msg, err := s.GetLatestMessage(context.Background(), &message.GetLatestMessagePayload{
+		QueueID: q.ID,
+		User:    usr,
+	})
+	assert.Nil(t, msg)
+	assert.Error(t, err)
+}
+
+func TestGetLatestMessage(t *testing.T) {
+	s, client, l := CreateMessageService(t)
+	defer client.Close()
+	defer l.Sync()
+
+	usr, err := createUser(client)
+	assert.NotNil(t, usr)
+	assert.NoError(t, err)
+
+	q, err := createQueue(client, usr, "some-queue")
+	assert.NotNil(t, q)
+	assert.NoError(t, err)
+
+	msg1, err := createQueueMessage(client, usr, q, "some message")
+	assert.NotNil(t, msg1)
+	assert.NoError(t, err)
+
+	msg2, err := s.GetLatestMessage(context.Background(), &message.GetLatestMessagePayload{
+		QueueID: q.ID,
+		User:    usr,
+	})
+	assert.NotNil(t, msg2)
+	assert.NoError(t, err)
+	assert.Equal(t, msg1.ID, msg2.ID)
+	assert.Equal(t, schema.QueueMessageStatusProcessing, msg2.Status)
+
+}
